25978cf49745517824a3a47496b05cc1b235842a
Nahum <nicolasdrv10@gmail.com>
2024-10-17 11:38:59 -0500

    Creado clase Regla VALIDACION, Ganimedes, Calisto y Validador

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,41 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractclassmethod, abstractmethod
+from typing import Protocol
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, clave: str):
+        self.clave: str = clave
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+    def _validar_longitud(self, clave: str) -> bool:
+        return len(clave) > 5
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave: str) -> bool:
+        return any(c.isdigit() for c in clave)
+
+
+
+class ReglaValidacionGanimedes:
+    pass
+
+
+class ReglaValidacionCalisto:
+    pass
+
+
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        pass
+
+    def _es_valida(self, clave: str) -> bool:
+        pass
